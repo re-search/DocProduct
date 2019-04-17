2969229501e67d7f4a61d90df41636aecd134481
JayYip <junpang.yip@gmail.com>
2019-04-17 17:38:14 +0800

    [feature] Add dataset for bert
    - Minor change in bert tokenizer to make it tf2.0 compatible

--- dataset.py
+++ dataset.py
@@ -1,32 +1,31 @@
 import os
+from glob import glob
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
+SEED = 42
+
 
 def create_generator_for_ffn(
         data_dir,
-        file_list=[
-            "ehealthforumQAs.csv",
-            "icliniqQAs.csv",
-            "questionDoctorQAs.csv",
-            "webmdQAs.csv",
-            "healthtapQAs.csv"],
         mode='train'):
 
-    for file_name in file_list:
-        full_file_path = os.path.join(data_dir, file_name)
+    file_list = glob(os.path.join(data_dir, '*.csv'))
+
+    for full_file_path in file_list:
+        # full_file_path = os.path.join(data_dir, file_name)
         if not os.path.exists(full_file_path):
             raise FileNotFoundError("File %s not found" % full_file_path)
         df = pd.read_csv(full_file_path)
 
         # so train test split
         if mode == 'train':
-            df, _ = train_test_split(df, test_size=0.2)
+            df, _ = train_test_split(df, test_size=0.2, random_state=SEED)
         else:
-            _, df = train_test_split(df, test_size=0.2)
+            _, df = train_test_split(df, test_size=0.2, random_state=SEED)
 
         for _, row in df.iterrows():
             q_vectors = np.fromstring(row.question_bert.replace(
@@ -48,12 +47,6 @@ def create_generator_for_ffn(
 
 def create_dataset_for_ffn(
         data_dir,
-        file_list=[
-            "ehealthforumQAs.csv",
-            "icliniqQAs.csv",
-            "questionDoctorQAs.csv",
-            "webmdQAs.csv",
-            "healthtapQAs.csv"],
         mode='train',
         hidden_size=768,
         shuffle_buffer=10000,
@@ -62,7 +55,6 @@ def create_dataset_for_ffn(
 
     def gen(): return create_generator_for_ffn(
         data_dir=data_dir,
-        file_list=file_list,
         mode=mode)
 
     output_types = {
@@ -75,6 +67,201 @@ def create_dataset_for_ffn(
         'a_vectors': [hidden_size],
     }
 
+    if mode == 'train':
+        output_types.update({'labels': tf.int32})
+        output_shapes.update({'labels': []})
+
+    dataset = tf.data.Dataset.from_generator(
+        generator=gen,
+        output_types=output_types,
+        output_shapes=output_shapes
+    )
+    if mode == 'train':
+        dataset = dataset.shuffle(shuffle_buffer)
+
+    dataset = dataset.prefetch(prefetch)
+
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+
+class PaddingInputExample(object):
+    """Fake example so the num input examples is a multiple of the batch size.
+    When running eval/predict on the TPU, we need to pad the number of examples
+    to be a multiple of the batch size, because the TPU requires a fixed batch
+    size. The alternative is to drop the last batch, which is bad because it means
+    the entire output data won't be generated.
+    We use this class instead of `None` because treating `None` as padding
+    battches could cause silent errors.
+    """
+
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        """Constructs a InputExample.
+    Args:
+      guid: Unique id for the example.
+      text_a: string. The untokenized text of the first sequence. For single
+        sequence tasks, only this sequence must be specified.
+      text_b: (Optional) string. The untokenized text of the second sequence.
+        Only must be specified for sequence pair tasks.
+      label: (Optional) string. The label of the example. This should be
+        specified for train and dev examples, but not for test examples.
+    """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
+def convert_single_example(tokenizer, example, max_seq_length=256):
+    """Converts a single `InputExample` into a single `InputFeatures`."""
+
+    if isinstance(example, PaddingInputExample):
+        input_ids = [0] * max_seq_length
+        input_mask = [0] * max_seq_length
+        segment_ids = [0] * max_seq_length
+        label = 0
+        return input_ids, input_mask, segment_ids, label
+
+    tokens_a = tokenizer.tokenize(example.text_a)
+    if len(tokens_a) > max_seq_length - 2:
+        tokens_a = tokens_a[0: (max_seq_length - 2)]
+
+    tokens = []
+    segment_ids = []
+    tokens.append("[CLS]")
+    segment_ids.append(0)
+    for token in tokens_a:
+        tokens.append(token)
+        segment_ids.append(0)
+    tokens.append("[SEP]")
+    segment_ids.append(0)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    while len(input_ids) < max_seq_length:
+        input_ids.append(0)
+        input_mask.append(0)
+        segment_ids.append(0)
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    return input_ids, input_mask, segment_ids, example.label
+
+
+def convert_examples_to_features(tokenizer, examples, max_seq_length=256):
+    """Convert a set of `InputExample`s to a list of `InputFeatures`."""
+
+    input_ids, input_masks, segment_ids, labels = [], [], [], []
+    for example in examples:
+        input_id, input_mask, segment_id, label = convert_single_example(
+            tokenizer, example, max_seq_length
+        )
+        input_ids.append(input_id)
+        input_masks.append(input_mask)
+        segment_ids.append(segment_id)
+        labels.append(label)
+    return (
+        np.squeeze(np.array(input_ids)),
+        np.squeeze(np.array(input_masks)),
+        np.squeeze(np.array(segment_ids)),
+        np.array(labels).reshape(-1, 1),
+    )
+
+
+def convert_text_to_feature(text, tokenizer, max_seq_length):
+    example = InputExample(
+        guid=None, text_a=text)
+    features = convert_examples_to_features(
+        tokenizer, [example], max_seq_length)
+    return features
+
+
+def create_generator_for_bert(
+        data_dir,
+        tokenizer,
+        mode='train',
+        max_seq_length=256):
+    file_list = glob(os.path.join(data_dir, '*.csv'))
+    for full_file_path in file_list:
+        # full_file_path = os.path.join(data_dir, file_name)
+        if not os.path.exists(full_file_path):
+            raise FileNotFoundError("File %s not found" % full_file_path)
+        df = pd.read_csv(full_file_path)
+
+        # so train test split
+        if mode == 'train':
+            df, _ = train_test_split(df, test_size=0.2, random_state=SEED)
+        else:
+            _, df = train_test_split(df, test_size=0.2, random_state=SEED)
+
+        for _, row in df.iterrows():
+            q_features = convert_text_to_feature(
+                row.question, tokenizer, max_seq_length)
+            a_features = convert_text_to_feature(
+                row.answer, tokenizer, max_seq_length)
+            if mode == 'train':
+                yield {
+                    "q_input_ids": q_features[0],
+                    "q_input_masks": q_features[1],
+                    "q_segment_ids": q_features[2],
+                    "a_input_ids": a_features[0],
+                    "a_input_masks": a_features[1],
+                    "a_segment_ids": a_features[2],
+                    "labels": 1
+                }
+            else:
+                yield {
+                    "q_input_ids": q_features[0],
+                    "q_input_masks": q_features[1],
+                    "q_segment_ids": q_features[2],
+                    "a_input_ids": a_features[0],
+                    "a_input_masks": a_features[1],
+                    "a_segment_ids": a_features[2],
+                }
+
+
+def create_dataset_for_bert(
+        data_dir,
+        tokenizer=None,
+        mode='train',
+        max_seq_length=256,
+        shuffle_buffer=10000,
+        prefetch=128,
+        batch_size=32):
+
+    def gen(): return create_generator_for_bert(
+        data_dir=data_dir,
+        tokenizer=tokenizer,
+        mode=mode,
+        max_seq_length=max_seq_length)
+
+    output_types = {
+        'q_input_ids': tf.int32,
+        'q_input_masks': tf.int32,
+        'q_segment_ids': tf.int32,
+        'a_input_ids': tf.int32,
+        'a_input_masks': tf.int32,
+        'a_segment_ids': tf.int32
+    }
+    output_shapes = {
+        'q_input_ids': [max_seq_length],
+        'q_input_masks': [max_seq_length],
+        'q_segment_ids': [max_seq_length],
+        'a_input_ids': [max_seq_length],
+        'a_input_masks': [max_seq_length],
+        'a_segment_ids': [max_seq_length]
+    }
     if mode == 'train':
         output_types.update({'labels': tf.int32})
         output_shapes.update({'labels': []})
