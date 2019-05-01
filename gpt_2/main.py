60cf17dd8f83112bba3398a0810e954906bdf7e0
ash3n <33986672+ash3n@users.noreply.github.com>
2019-04-30 22:52:35 -0500

    Update GPT-2 main and .gitignore

--- gpt_2/main.py
+++ gpt_2/main.py
@@ -4,13 +4,13 @@ tf.disable_v2_behavior()
 import gpt2
 
 # make sure healthtapQAs.txt is in /data/
-txt_path = 'gpt2/data/healthtapQAs.txt'
+txt_path = 'data/healthtapQAs.txt'
 
 if not os.path.exists('models/117M'):
     gpt2.download_gpt2()
 
 sess = gpt2.start_tf_sess()
-gpt2.finetune(sess, txt_path, steps=1000)
+gpt2.finetune(sess, txt_path, steps=100, batch_size=4)
 
-gpt2.load_gpt2(sess)
+#gpt2.load_gpt2(sess)
 gpt2.generate(sess, prefix="`QUESTION: What is the best treatment for the flu? `ANSWER:")
