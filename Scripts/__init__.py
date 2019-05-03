9be84e7631c68ba1d7b7b5431b675c9f8eba785d
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-05-03 07:34:38 -0700

    Update __init__.py

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -1,13 +1,13 @@
-from .dataset
-from .get_data
-from .inference_question_to_topk
-from .loss
-from .metrics
-from .models
-from .predictor
-from .tokenization
-from .train_bertffn
-from .train_data_to_embedding
-from .train_faiss_topk_to_gpt2
-from .train_ffn
+from .dataset import *
+from .get_data import *
+from .inference_question_to_topk import *
+from .loss import *
+from .metrics import *
+from .models import *
+from .predictor import *
+from .tokenization import *
+from .train_bertffn import *
+from .train_data_to_embedding import *
+from .train_faiss_topk_to_gpt2 import *
+from .train_ffn import *
 
