01bcad5e8fa04f3f9bb27187d55fc93208b67ecb
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-05-02 19:46:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description='None so far',
     long_description_content_type='text/markdown',
     python_requires = '>=3.6.0'
-    packages=['MedicalQA'],   #Not sure if this is right
+    packages=setuptools.find_packages(),   #Not sure if this is right
     install_requires=['numpy', 'matplotlib', 'tensorflow', 'Keras', 'keras-pos-embd==0.9.0', 'keras-transformer==0.21.0', 'tqdm', 'csv', 'faiss', 'sklearn', 'urllib', 're', 'pycurl', 'hashlib', 'os', 'collections', 'time', 'multiprocessing', 'unicodedata', 'six', 'argparse', 'glob', 'math'] #Need help with this too
     classifiers=(
         "Programming Language :: Python :: 2.7",
