4f3391792a4563ecc198cc60efa2d764021d3f40
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-05-02 20:01:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
     long_description='None so far',
     long_description_content_type='text/markdown',
     python_requires = '>=3.6.0'
-    packages=setuptools.find_packages(),   #Not sure if this is right
-    install_requires=['numpy', 'matplotlib', 'tensorflow', 'Keras', 'keras-pos-embd==0.9.0', 'keras-transformer==0.21.0', 'tqdm', 'csv', 'faiss', 'sklearn', 'urllib', 're', 'pycurl', 'hashlib', 'os', 'collections', 'time', 'multiprocessing', 'unicodedata', 'six', 'argparse', 'glob', 'math'] #Need help with this too
+    packages=setuptools.find_packages(),   
+    install_requires=['numpy', 'matplotlib', 'tensorflow', 'Keras', 'keras-pos-embd==0.9.0', 'keras-transformer==0.21.0', 'tqdm', 'csv', 'faiss', 'sklearn', 'urllib', 're', 'pycurl', 'hashlib', 'os', 'collections', 'time', 'multiprocessing', 'unicodedata', 'six', 'argparse', 'glob', 'math'] 
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
