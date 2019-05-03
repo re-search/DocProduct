0eac2c5d662d0550916149a0162904c9443f6833
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-05-02 19:15:14 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
+
+setup(
+    name='MedicalQA',
+    version='0.01.0',
+    packages=find_packages(),
+    url='https://github.com/Santosh-Gupta/MedicalQA',
+    license='MIT',
+    author='MedicalQATeam',
+    author_email='SanGupta.ML@gmail.com',
+    description='BERT in TF2.0 for Medical QA info retrieval + GPT2 for answer generation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
