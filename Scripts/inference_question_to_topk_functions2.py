39096fcd88857493045319661b296b589820198f
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-05-03 17:42:07 -0700

    Update inference_question_to_topk_functions2.py

--- Scripts/inference_question_to_topk_functions2.py
+++ Scripts/inference_question_to_topk_functions2.py
@@ -13,15 +13,19 @@ class createEmbeds:
             self.bert_ffn_weight_file = bert_ffn_weight_file
             self.embedding_file = embedding_file
 
-    qa_embed = QAEmbed(
-        pretrained_path=self.pretrained_path,
-        ffn_weight_file=self.ffn_weight_file,
-        bert_ffn_weight_file=self.bert_ffn_weight_file
-    )
+    def get_qa_embed(self):
+        qa_embed = QAEmbed(
+            pretrained_path=self.pretrained_path,
+            ffn_weight_file=self.ffn_weight_file,
+            bert_ffn_weight_file=self.bert_ffn_weight_file
+        )
+        return qa_embed
 
-    faiss_topk = FaissTopK(self.embedding_file)
+    def get_faiss_topk(self):
+        faiss_topk = FaissTopK(self.embedding_file)
+        return faiss_topk
 
-    doc = RetreiveQADoc(qa_embed, faiss_topk)
+    doc = RetreiveQADoc(get_qa_embed(), get_faiss_topk())
 
 #     print(doc.predict('i have a headache.',
 #                       search_by='answer', topk=5, answer_only=True))
