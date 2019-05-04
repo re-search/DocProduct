b979623100fffedc7b23aded1c8945f29b4ca371
ash3n <33986672+ash3n@users.noreply.github.com>
2019-05-04 18:37:31 -0500

    GPT-2 pre-tokenization support

--- gpt_2/gpt2/src/mqa_load_dataset.py
+++ gpt_2/gpt2/src/mqa_load_dataset.py
@@ -7,7 +7,7 @@ import tqdm
 import csv
 
 
-def load_dataset(enc, path, combine):
+def load_dataset(enc, path, combine, pretokenize=True):
     paths = []
     if os.path.isfile(path):
         # Simple file
@@ -25,37 +25,85 @@ def load_dataset(enc, path, combine):
 
     token_chunks = []
     raw_text = ''
-    for path in paths:
-        '''
-        if path.endswith('.npz'):
-            # Pre-encoded
-            with np.load(path) as npz:
-                for item in npz.files:
-                    token_chunks.append(npz[item])
-        else:
-            # Plain text
-            with open(path, 'r', encoding='utf8', errors='ignore') as fp:
-                raw_text += fp.read()
-            if len(raw_text) >= combine:
-                tokens = np.stack(enc.encode(raw_text))
+
+    if pretokenize:
+
+        pt_path = path.split('.')[0] + '_pretokenized.' + path.split('.')[1]
+
+        if not os.path.exists(pt_path):
+
+            print('Pretokenizing data..')
+
+            op = open(pt_path, 'w', encoding='utf8')
+            csv_writer = csv.writer(op, lineterminator = '\n')
+            
+            for path in paths:
+                with open(path, 'r', encoding='utf8', errors='ignore') as fp:
+
+                    csv_reader = csv.reader(fp)
+                    csv_iter = list(csv_reader)
+                    csv_writer.writerow(csv_iter[0] + ['tokens'])
+
+                    for j, sample in enumerate(tqdm.tqdm(csv_iter[1:])):
+                        line = '`QUESTION: %s `ANSWER: %s' % (sample[0], sample[1])
+                        for i in range(len(sample), 2, -2):
+                            line = '`QUESTION: %s `ANSWER: %s ' % (sample[i-2], sample[i-1]) + line
+                        line = line.replace('\n', '')
+                        tokens = list(np.stack(enc.encode(line)))
+                        csv_writer.writerow(sample + [tokens])
+                    
+                print('Pretokenization successful!')
+
+            op.close()
+
+        print('Loading pretokenized data..')
+        
+        with open(pt_path, 'r', encoding='utf8') as pt:
+            pt_reader = csv.reader(pt)
+            pt_iter = list(pt_reader)
+
+            for j, sample in enumerate(tqdm.tqdm(pt_iter[1:])):
+                #print(sample[-1])
+                #print('HOLY FUCKING SHIT')
+                tokens = np.asarray(sample[-1].strip('[]').replace(',', '').split(), dtype=np.int32)
                 token_chunks.append(tokens)
-                raw_text = ''
+
+    else:
+    
+        for path in paths:
+            '''
+            if path.endswith('.npz'):
+                # Pre-encoded
+                with np.load(path) as npz:
+                    for item in npz.files:
+                        token_chunks.append(npz[item])
             else:
-                raw_text += '<|endoftext|>'
+                # Plain text
+                with open(path, 'r', encoding='utf8', errors='ignore') as fp:
+                    raw_text += fp.read()
+                if len(raw_text) >= combine:
+                    tokens = np.stack(enc.encode(raw_text))
+                    token_chunks.append(tokens)
+                    raw_text = ''
+                else:
+                    raw_text += '<|endoftext|>'
+            '''
+            with open(path, 'r', encoding='utf8', errors='ignore') as fp:
+                csv_reader = csv.reader(fp)
+
+
+                for j, sample in enumerate(tqdm.tqdm(csv_reader)):
+                    line = '`QUESTION: %s `ANSWER: %s' % (sample[0], sample[1])
+                    for i in range(len(sample), 2, -2):
+                        line = '`QUESTION: %s `ANSWER: %s ' % (sample[i-2], sample[i-1]) + line
+                    tokens = np.stack(enc.encode(line))
+                    token_chunks.append(tokens)
         '''
-        with open(path, 'r', encoding='utf8', errors='ignore') as fp:
-            csv_reader = csv.reader(fp)
-            for j, sample in enumerate(tqdm.tqdm(csv_reader)):
-                line = '`QUESTION: %s `ANSWER: %s' % (sample[0], sample[1])
-                for i in range(len(sample), 2, -2):
-                    line = '`QUESTION: %s `ANSWER: %s ' % (sample[i-2], sample[i-1]) + line
-                tokens = np.stack(enc.encode(line))
-                token_chunks.append(tokens)
-    '''
-    if raw_text:
-        tokens = np.stack(enc.encode(raw_text))
-        token_chunks.append(tokens)
-    '''
+        if raw_text:
+            tokens = np.stack(enc.encode(raw_text))
+            token_chunks.append(tokens)
+        '''
+
     return token_chunks
 
 
