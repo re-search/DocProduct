50877925a1c23f52bfb7ca4e9f8f467c18df1b3c
ash3n <33986672+ash3n@users.noreply.github.com>
2019-05-04 19:02:34 -0500

    Update gpt_2/main.py

--- gpt_2/main.py
+++ gpt_2/main.py
@@ -3,6 +3,8 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import gpt2
 
+train = True
+
 # make sure healthtapQAs.txt is in /data/
 txt_path = 'data/healthtapQAs.txt'
 csv_path = 'data/GPT2_data_FFNN.csv'
@@ -11,7 +13,10 @@ if not os.path.exists('models/117M'):
     gpt2.download_gpt2()
 
 sess = gpt2.start_tf_sess()
-gpt2.finetune(sess, csv_path, steps=100000, batch_size=1)
 
-#gpt2.load_gpt2(sess)
+if train:
+    gpt2.finetune(sess, csv_path, steps=100000, batch_size=1)
+else:
+    gpt2.load_gpt2(sess)
+
 gpt2.generate(sess, prefix="`QUESTION: What is the best treatment for the flu? `ANSWER:")
