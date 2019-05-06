4aa625ae8ba40b343c1110b7bbce990104ead604
ash3n <33986672+ash3n@users.noreply.github.com>
2019-05-06 02:13:26 -0500

    Update main.py to retrain

--- gpt_2/main.py
+++ gpt_2/main.py
@@ -1,13 +1,14 @@
 import os.path
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-import gpt2
+from gpt_2 import gpt2
+
+tf.disable_eager_execution()
 
 train = True
 
 # make sure healthtapQAs.txt is in /data/
 txt_path = 'data/healthtapQAs.txt'
-csv_path = 'data/GPT2_data_FFNN.csv'
+csv_path = 'gpt2_train_data/bertffn_crossentropy.csv'
 
 if not os.path.exists('models/117M'):
     gpt2.download_gpt2()
@@ -15,7 +16,7 @@ if not os.path.exists('models/117M'):
 sess = gpt2.start_tf_sess()
 
 if train:
-    gpt2.finetune(sess, csv_path, steps=100000, batch_size=1)
+    gpt2.finetune(sess, csv_path, steps=30000, batch_size=1)
 else:
     gpt2.load_gpt2(sess)
 
