59e82bcbf12774ed945d4c045b238973f2310c16
JayYip <junpang.yip@gmail.com>
2019-05-07 11:38:53 +0800

    [refrac] Change embedding format to pkl

--- Scripts/predictor.py
+++ Scripts/predictor.py
@@ -151,21 +151,20 @@ class FaissTopK(object):
     def __init__(self, embedding_file):
         super(FaissTopK, self).__init__()
         self.embedding_file = embedding_file
-        self.df = pd.read_csv(self.embedding_file)
+        self.df = pd.read_pickle(self.embedding_file)
         self._get_faiss_index()
-        self.df.drop(columns=["Q_FFNN_embeds", "A_FFNN_embeds"], inplace=True)
+        # self.df.drop(columns=["Q_FFNN_embeds", "A_FFNN_embeds"], inplace=True)
 
     def _get_faiss_index(self):
-        with Pool(cpu_count()) as p:
-            question_bert = p.map(eval, self.df["Q_FFNN_embeds"].tolist())
-            answer_bert = p.map(eval, self.df["A_FFNN_embeds"].tolist())
-        # question_bert = self.df["Q_FFNN_embeds"].tolist()
-        # answer_bert = self.df["A_FFNN_embeds"].tolist()
-        question_bert = np.array(question_bert)
-        answer_bert = np.array(answer_bert)
-
-        question_bert = question_bert.astype('float32')
-        answer_bert = answer_bert.astype('float32')
+        # with Pool(cpu_count()) as p:
+        #     question_bert = p.map(eval, self.df["Q_FFNN_embeds"].tolist())
+        #     answer_bert = p.map(eval, self.df["A_FFNN_embeds"].tolist())
+        question_bert = self.df["Q_FFNN_embeds"].tolist()
+        self.df.drop(columns=["Q_FFNN_embeds"], inplace=True)
+        answer_bert = self.df["A_FFNN_embeds"].tolist()
+        self.df.drop(columns=["A_FFNN_embeds"], inplace=True)
+        question_bert = np.array(question_bert, dtype='float32')
+        answer_bert = np.array(answer_bert, dtype='float32')
 
         self.answer_index = faiss.IndexFlatIP(answer_bert.shape[-1])
 
@@ -173,6 +172,8 @@ class FaissTopK(object):
 
         self.answer_index.add(answer_bert)
         self.question_index.add(question_bert)
+
+        del answer_bert, question_bert
 
     def predict(self, q_embedding, search_by='answer', topk=5, answer_only=True):
         if search_by == 'answer':
