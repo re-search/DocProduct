279ce10077256fa8c3ee4fdba43287ad88a07505
Jay Yip <junpang.yip@gmail.com>
2019-05-16 03:30:33 +0000

    [feature] Add new generative predictor

--- docproduct/predictor.py
+++ docproduct/predictor.py
@@ -8,12 +8,13 @@ from tqdm import tqdm
 import pandas as pd
 from multiprocessing import Pool, cpu_count
 import faiss
+import json
 
 from docproduct.dataset import convert_text_to_feature
 from docproduct.models import MedicalQAModelwithBert
 from docproduct.tokenization import FullTokenizer
 from keras_bert.loader import checkpoint_loader
-# from gpt_2 import gpt2
+import gpt2_estimator
 
 
 def load_weight(model, bert_ffn_weight_file=None, ffn_weight_file=None):
@@ -214,58 +215,83 @@ class RetreiveQADoc(object):
         return embedding
 
 
-# class GenerateQADoc(object):
-#     def __init__(self,
-#                  pretrained_path='pubmed_pmc_470k/',
-#                  ffn_weight_file=None,
-#                  bert_ffn_weight_file='models/bertffn_crossentropy/bertffn',
-#                  embedding_file='qa_embeddings/bertffn_crossentropy.zip'
-#                  ):
-#         super(GenerateQADoc, self).__init__()
-#         tf.compat.v1.disable_eager_execution()
-#         self.sess = gpt2.start_tf_sess()
-#         gpt2.load_gpt2(self.sess)
-#         self.embed_sess = gpt2.start_tf_sess()
-#         with self.embed_sess.as_default():
-#             self.qa_embed = QAEmbed(
-#                 pretrained_path=pretrained_path,
-#                 ffn_weight_file=ffn_weight_file,
-#                 bert_ffn_weight_file=bert_ffn_weight_file,
-#                 with_answer=False,
-#                 load_pretrain=False
-#             )
+class GenerateQADoc(object):
+    def __init__(self,
+                 pretrained_path='pubmed_pmc_470k/',
+                 ffn_weight_file=None,
+                 bert_ffn_weight_file='models/bertffn_crossentropy/bertffn',
+                 gpt2_weight_file='models/gpt2',
+                 embedding_file='qa_embeddings/bertffn_crossentropy.zip'
+                 ):
+        super(GenerateQADoc, self).__init__()
+        tf.compat.v1.disable_eager_execution()
+        session_config = tf.compat.v1.ConfigProto(
+            allow_soft_placement=True)
+        session_config.gpu_options.allow_growth = False
+        config = tf_estimator.estimator.RunConfig(
+            session_config=session_config)
+        self.batch_size = 1
+        gpt2_model_fn = gpt2_estimator.get_gpt2_model_fn(
+            accumulate_gradients=5,
+            learning_rate=0.1,
+            length=512,
+            batch_size=self.batch_size,
+            temperature=0.7,
+            top_k=0
+        )
+        hparams = gpt2_estimator.default_hparams()
+        with open(os.path.join(pretrained_path, 'hparams.json')) as f:
+            hparams.override_from_dict(json.load(f))
+        self.estimator = tf.estimator.Estimator(
+            gpt2_model_fn,
+            model_dir=gpt2_weight_file,
+            params=hparams,
+            config=config)
 
-#         self.faiss_topk = FaissTopK(embedding_file)
+        config = tf.compat.v1.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.embed_sess = tf.compat.v1.Session(config=config)
+        with self.embed_sess.as_default():
+            self.qa_embed = QAEmbed(
+                pretrained_path=pretrained_path,
+                ffn_weight_file=ffn_weight_file,
+                bert_ffn_weight_file=bert_ffn_weight_file,
+                with_answer=False,
+                load_pretrain=False
+            )
 
-#     def _get_gpt2_inputs(self, question, questions, answers):
-#         assert len(questions) == len(answers)
-#         line = '`QUESTION: %s `ANSWER: ' % question
-#         for q, a in zip(questions, answers):
-#             line = '`QUESTION: %s `ANSWER: %s ' % (q, a) + line
-#         return line
+        self.faiss_topk = FaissTopK(embedding_file)
 
-#     def predict(self, questions, search_by='answer', topk=5, answer_only=False):
-#         embedding = self.qa_embed.predict(
-#             questions=questions, dataset=False).eval(session=self.embed_sess)
-#         if answer_only:
-#             topk_answer = self.faiss_topk.predict(
-#                 embedding, search_by, topk, answer_only)
-#         else:
-#             topk_question, topk_answer = self.faiss_topk.predict(
-#                 embedding, search_by, topk, answer_only)
+    def _get_gpt2_inputs(self, question, questions, answers):
+        assert len(questions) == len(answers)
+        line = '`QUESTION: %s `ANSWER: ' % question
+        for q, a in zip(questions, answers):
+            line = '`QUESTION: %s `ANSWER: %s ' % (q, a) + line
+        return line
 
-#         gpt2_input = self._get_gpt2_inputs(
-#             questions, topk_question, topk_answer)
-#         raw_output = gpt2.generate(
-#             self.sess, prefix=gpt2_input, return_as_list=True, include_prefix=False)
-#         original_line = '`QUESTION: %s `ANSWER: ' % questions
-#         output_list = []
-#         for output_ind, output_chunk in enumerate(raw_output[0].split(original_line)):
-#             if output_ind == 0:
-#                 pass
-#             else:
-#                 output_list.append(output_chunk.split('`QUESTION')[0])
+    def predict(self, questions, search_by='answer', topk=5, answer_only=False):
+        embedding = self.qa_embed.predict(
+            questions=questions, dataset=False).eval(session=self.embed_sess)
+        if answer_only:
+            topk_answer = self.faiss_topk.predict(
+                embedding, search_by, topk, answer_only)
+        else:
+            topk_question, topk_answer = self.faiss_topk.predict(
+                embedding, search_by, topk, answer_only)
 
-#         # clipped_output = raw_output[0].split(
-#         #     '`QUESTION')[1].split('`ANSWER:')[1]
-#         return output_list
+        gpt2_input = self._get_gpt2_inputs(
+            questions, topk_question, topk_answer)
+        gpt2_pred = self.estimator.predict(
+            gpt2_estimator.predict_input_fn(inputs=gpt2_input))
+        raw_output = gpt2_estimator.predictions_parsing(gpt2_pred)
+        # original_line = '`QUESTION: %s `ANSWER: ' % questions
+        # output_list = []
+        # for output_ind, output_chunk in enumerate(raw_output[0].split(original_line)):
+        #     if output_ind == 0:
+        #         pass
+        #     else:
+        #         output_list.append(output_chunk.split('`QUESTION')[0])
+
+        # clipped_output = raw_output[0].split(
+        #     '`QUESTION')[1].split('`ANSWER:')[1]
+        return raw_output
